3d46d306bf0e2a86858b3306c284668a29a3915d
Hunter Han <hunter602353@yahoo.com>
2019-03-23 23:02:36 -0500

    Some progress made I CANT FIGURE THIS OUT

--- app/src/main/java/imalols/com/skymobileandroid/SkywardLoginActivity.kt
+++ app/src/main/java/imalols/com/skymobileandroid/SkywardLoginActivity.kt
@@ -1,43 +1,77 @@
 package imalols.com.skymobileandroid
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
-import android.annotation.TargetApi
-import android.content.pm.PackageManager
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
 import android.app.LoaderManager.LoaderCallbacks
-import android.content.CursorLoader
+import android.content.Context
 import android.content.Loader
 import android.database.Cursor
-import android.net.Uri
-import android.os.AsyncTask
-import android.os.Build
+import android.graphics.Typeface
 import android.os.Bundle
-import android.provider.ContactsContract
-import android.text.TextUtils
-import android.view.View
-import android.view.inputmethod.EditorInfo
-import android.widget.ArrayAdapter
+import android.support.v7.app.AppCompatActivity
+import android.util.TypedValue
+import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.LinearLayout
 import android.widget.TextView
-
-import java.util.ArrayList
-import android.Manifest.permission.READ_CONTACTS
-
 import kotlinx.android.synthetic.main.activity_skyward_login.*
+import java.lang.Exception
 
 /**
  * A login screen that offers login via email/password.
  */
 class SkywardLoginActivity : AppCompatActivity(), LoaderCallbacks<Cursor> {
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
+    var SkywardWebsite: WebView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        println("HEHLELELO")
+        setContentView(R.layout.activity_skyward_login)
 
-        println("YIFAN MA")
+        val Skyward = WebView(this)
+
+        Skyward.layoutParams = parentView.layoutParams
+        email_login_form.addView(Skyward)
+        Skyward.loadUrl("https://skyward-fbprod.iscorp.com/scripts/wsisa.dll/WService=wsedufortbendtx/seplog01.w")
+        Skyward.settings.javaScriptEnabled = true
+        Skyward.settings.javaScriptCanOpenWindowsAutomatically = true
+        Skyward.settings.setSupportMultipleWindows(true)
+
+        email_sign_in_button.setOnClickListener{
+            AttemptLoginToSkyward(username.text.toString(), password.text.toString())
+        }
+
+        SkywardWebsite = Skyward
+    }
+
+    fun AttemptLoginToSkyward(Username: String, Passcode: String){
+        val Javascript = "login.value = \"$Username\"; password.value = \"$Passcode\"; bLogin.click();"
+        SkywardWebsite!!.evaluateJavascript(Javascript){
+            println("This isn't supposed to be here")
+            val javascrip1t = """
+                        function check(){
+                        if(msgBodyCol.textContent == "Invalid login or password."){
+                        msgBtn1.click()
+                        return "fail"
+                        } else if(dMessage.attributes["style"].textContent.includes("visibility: visible")) {
+                        msgBtn1.click()
+                        return "clicked"
+                        }else if(document.querySelector(".ui-widget").textContent.includes("Please wait")){
+                        return "Loading"
+                        }else{
+                        return "oop"
+                        }
+                        }
+                        check()
+                        """
+            SkywardWebsite!!.evaluateJavascript(javascrip1t){it
+                try{
+                    println(it + "WHY")
+                }catch (e: Exception){
+                    println("Well that was an error")
+                }
+            }
+        }
     }
 
     override fun onCreateLoader(p0: Int, p1: Bundle?): Loader<Cursor> {
